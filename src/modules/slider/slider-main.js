018562d1af3b8fea7f467dcdfc04a058f8215e7c
Vadim Zinchenko <vadim.zinchenco@mail.ru>
2022-08-15 23:24:24 +0200

    add class forms & add services 'request'

---
+++ src/modules/slider/slider-main.js
@@ -0,0 +1,57 @@
+import Slider from './slider';
+
+export default class MainSlider extends Slider {
+  // eslint-disable-next-line no-useless-constructor
+  constructor(btns) {
+    super(btns);
+  }
+
+  showSlides(n) {
+    if (n > this.slides.length) {
+      this.slideIndex = 1;
+    }
+    if (n < 1) {
+      this.slideIndex = this.slides.length;
+    }
+
+    try {
+      this.hanson.style.opacity = '0';
+      if (n === 3) {
+        this.hanson.classList.add('animated');
+        setTimeout(() => {
+          this.hanson.classList.add('fadeInUp');
+        }, 3000);
+      } else {
+        this.hanson.classList.remove('fadeInUp');
+      }
+    } catch (e) {}
+
+    this.slides.forEach((slide) => {
+      slide.style.display = 'none';
+    });
+
+    this.slides[this.slideIndex - 1].style.display = 'block';
+  }
+
+  plusSlides(n) {
+    this.showSlides(this.slideIndex += n);
+  }
+
+  render() {
+    try {
+      this.hanson = document.querySelector('.hanson');
+    } catch (e) {}
+
+    this.btns.forEach((btn) => {
+      btn.addEventListener('click', () => {
+        this.plusSlides(1);
+      });
+      btn.parentNode.previousElementSibling.addEventListener('click', (e) => {
+        e.preventDefault();
+        this.slideIndex = 1;
+        this.showSlides(this.slideIndex);
+      });
+    });
+    this.showSlides(this.slideIndex);
+  }
+}
---
+++ src/modules/slider/slider.js
@@ -0,0 +1,15 @@
+export default class Slider {
+  constructor({
+    container = null, btns = null, next = null, prev = null, activeClass = '', animate, autoPlay,
+  } = {}) {
+    this.container = document.querySelector(container);
+    this.slides = this.container.children;
+    this.btns = document.querySelectorAll(btns);
+    this.prev = document.querySelector(prev);
+    this.next = document.querySelector(next);
+    this.activeClass = activeClass;
+    this.animate = animate;
+    this.autoPlay = autoPlay;
+    this.slideIndex = 1;
+  }
+}
